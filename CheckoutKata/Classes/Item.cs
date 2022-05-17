c2a0096978d837b84fb98fbaa618897824270456
Ian Parker <ianparker2007@yahoo.co.uk>
2022-05-17 21:23:57 +0100

    Created implementation of IPromotion and IItem

---
+++ CheckoutKata/Classes/Item.cs
@@ -0,0 +1,15 @@
+﻿using CheckoutKata.Interfaces;
+
+namespace CheckoutKata.Classes;
+
+public class Item : IItem
+{
+    public string ItemSku { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int Quantity { get; set; }
+    public List<IPromotion> Promotions { get; set; }
+    public decimal GetCost()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CheckoutKata/Classes/Promotion.cs
@@ -0,0 +1,11 @@
+﻿using CheckoutKata.Enums;
+using CheckoutKata.Interfaces;
+
+namespace CheckoutKata.Classes;
+
+public class Promotion : IPromotion
+{
+    public EPromotionType Type { get; set; }
+    public int Threshold { get; set; }
+    public int ModifierValue { get; set; }
+}
